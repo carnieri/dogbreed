37d8d7564a0365a0d0b58aadc4e3516b359b15c1
Ricardo Carnieri <carnieri@gmail.com>
2021-11-12 15:41:14 -0300

    Remove duplicate images from train

--- cleanup_dataset.py
+++ cleanup_dataset.py
@@ -1,14 +1,43 @@
-"""Remove totally black images from dataset"""
+"""Remove totally black images and duplicates from dataset."""
 
+import hashlib
 import os
 from pathlib import Path
 
 import cv2
 import numpy as np
 
-path = Path("dogs/train")
-for p in path.glob("**/*.jpg"):
-    img = cv2.imread(str(p))
-    if np.alltrue(img == 0):
-        print(f"{p} is totally black, deleting")
-        os.remove(p)
+
+def remove_black_images(path):
+    path = Path(path)
+    for p in path.glob("**/*.jpg"):
+        img = cv2.imread(str(p))
+        if np.alltrue(img == 0):
+            print(f"{p} is totally black, deleting")
+            os.remove(p)
+
+
+def remove_duplicates(path):
+    path = Path(path)
+    hash_dict = {}
+    for p in path.glob("**/*.jpg"):
+        h = hashlib.sha256()
+        with open(p, "rb") as f:
+            buffer = f.read()
+            h.update(buffer)
+        digest = h.hexdigest()
+        if digest in hash_dict:
+            other_path = hash_dict[digest]
+            print(f"duplicate hash: {digest} {p} {other_path}")
+            if p.parent.name == other_path.parent.name:
+                # duplicate is in the same directory (same breed)
+                # it can safely be deleted
+                print(f"deleting file {p}")
+                os.remove(p)
+        else:
+            hash_dict[digest] = p
+
+
+if __name__ == "__main__":
+    remove_black_images(Path("dogs/train"))
+    remove_duplicates(Path("dogs/train"))
